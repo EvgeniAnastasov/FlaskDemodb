40b38774ffbcb0fc0639de1a0dd257dca2a5feda
Evgeni Anastasov <one_evg@mail.bg>
2023-03-07 22:30:56 +0200

    added .env file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from decouple import config
 from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Resource, Api
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://postgres:1123QwER@localhost:5432/store'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}" \
+    f"@localhost:{config('DB_PORT')}/{config('DB_NAME')}"
 db = SQLAlchemy(app)
 api = Api(app)
 migrate = Migrate(app, db)
